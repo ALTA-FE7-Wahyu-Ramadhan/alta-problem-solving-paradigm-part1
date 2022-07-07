89090397c0bcea0fb49fd829631ed45d4b8066eb
wramadhan <wahyuramadhan0549@gmail.com>
2022-07-08 00:16:10 +0700

    problem1 was cleared

--- Part1/problem1/problem1.js
+++ Part1/problem1/problem1.js
@@ -1,17 +1,23 @@
 function simpleEquations(a, b, c) {
   // Your code here
-  // x + y + z = A
-  // xyz = B
-  // x^2 + y^2 + z^2 = C
-  let penjumlahan = 0;
+  let penjumlahan = [];
   let perkalian = 0;
   let pangkat = 0;
-  for(let i<a; i>0; i--){
-    if(a%i==0){
-      penjumlahan+=i
+  if(a<=1){
+    return 'no solution';
+  } else{
+  for(let i=a-1; i>0; i--){
+    if(a%i==0) {
+      penjumlahan.unshift(i);
+      a-=i;
+    } else if(a==0){
+      break
     }
   }
+  return penjumlahan.toString().replace(","," ", -1)
 }
-simpleEquations(1, 2, 3)  // no solution
-simpleEquations(6, 6, 14) // 1 2 3
+}
+console.log(simpleEquations(1, 2, 3))  // no solution
+console.log(simpleEquations(6, 6, 14)) // 1 2 3
+
 module.exports = simpleEquations;
