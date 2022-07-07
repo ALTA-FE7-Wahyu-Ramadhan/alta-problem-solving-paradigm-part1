08a7526398d397d7fea276afa45318988c162f23
wramadhan <wahyuramadhan0549@gmail.com>
2022-07-08 01:12:03 +0700

    istirahat dulu

--- Part1/problem1/problem1.js
+++ Part1/problem1/problem1.js
@@ -8,33 +8,33 @@ function simpleEquations(a, b, c) {
   let perkalian = [];
   let pangkat = 0;
 
-  if(a<=1){
-    return 'no solution';
-  } else{
-  for(let i=a-1; i>0; i--){
-    if(a%i==0) {
-      penjumlahan.unshift(i);
-      a-=i;
-    } else if(a==0){
-      break
+  if (a <= 1) {
+    return "no solution";
+  } else {
+    for (let i = a - 1; i > 0; i--) {
+      if (a % i == 0) {
+        penjumlahan.unshift(i);
+        a -= i;
+      } else if (a == 0) {
+        break;
+      }
+    }
+    // return penjumlahan.toString().replace(","," ", -1)
+    penjumlahan;
+  }
+  for (let j = b - 1; j > 0; j--) {
+    if (b % j == 0) {
+      perkalian.unshift(j);
+      b /= j;
+    } else if (b == 1) {
+      perkalian.unshift(1);
+      break;
     }
   }
-  // return penjumlahan.toString().replace(","," ", -1)
-  penjumlahan
+  perkalian;
+  return (penjumlahan, perkalian).toString().replace(/,/g, " ");
 }
-for(let j=b-1; j>0; j--){
-  if(b%j==0){
-    perkalian.unshift(j);
-    b/=j;
-  }else if(b==1){
-    perkalian.unshift(1);
-    break;
-  }
-}
-perkalian
-return (penjumlahan,perkalian).toString().replace(/,/g, ' ');
-}
-console.log(simpleEquations(1, 2, 3))  // no solution
-console.log(simpleEquations(6, 6, 14)) // 1 2 3
+console.log(simpleEquations(1, 2, 3)); // no solution
+console.log(simpleEquations(6, 6, 14)); // 1 2 3
 
 module.exports = simpleEquations;
--- Part1/problem2/problem2.js
+++ Part1/problem2/problem2.js
@@ -1,5 +1,28 @@
 function moneyChange(money) {
   // Your code here
+  //rumuskan uangnya
+  let arrUang = [];
+  let maxPecahan = 10000;
+  for (let uang = 1; uang <= maxPecahan; uang++) {
+    if (uang == 1) {
+      arrUang.push(uang);
+    } else if (uang % maxPecahan == 0) {
+      arrUang.push(uang);
+    } else if (uang == maxPecahan / 2 ||
+    uang == maxPecahan / 5 || uang == maxPecahan / 10 || uang == maxPecahan / 20 || uang == maxPecahan / 50 || uang == maxPecahan / 100)
+    {
+      arrUang.push(uang);
+    
+  }
+  return arrUang;
+  /*let angka=[]
+  for(let i=1; i<money; i++){
+  }*/
 }
-
+console.log(moneyChange());
+// console.log(moneyChange(123))   // [100 20 1 1 1]
+// console.log(moneyChange(432))   // [200 200 20 10 1 1]
+// console.log(moneyChange(543))   // [500, 20, 20, 1, 1, 1]
+// console.log(moneyChange(7752))  // [5000, 2000, 500, 200, 50, 1, 1]
+// console.log(moneyChange(15321)) // [10000 5000 200 100 20 1]
 module.exports = moneyChange;
